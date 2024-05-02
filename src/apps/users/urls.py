56a17677bffe0857e7808deea81f1fa58f7af7b0
t.tatiyev <t.tatiyev@trafficwave.kz>
2024-05-02 18:39:36 +0500

    add is authenticated endpoint

--- src/apps/users/urls.py
+++ src/apps/users/urls.py
@@ -5,6 +5,7 @@ from rest_framework_simplejwt.views import (
     TokenVerifyView,
 )
 
+from .views import IsAuthenticatedView
 
 app_name = 'users'
 
@@ -13,4 +14,5 @@ urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('is-authenticated/', IsAuthenticatedView.as_view(), name='is_authenticated'),
 ]
